53180df53fa52dce04d266716f6ea2eabfe80c3c
achrafaitibba <aitibbaachraf@gmail.com>
2024-04-16 18:18:33 +0100

    implemented getMembersCount method

--- src/main/java/net/techbridges/telegdash/telegdashTelethonClientGateway/ApiGateway.java
+++ src/main/java/net/techbridges/telegdash/telegdashTelethonClientGateway/ApiGateway.java
@@ -3,10 +3,14 @@ package net.techbridges.telegdash.telegdashTelethonClientGateway;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
-
+import java.util.HashMap;
 
 
 @RestController
@@ -20,6 +24,24 @@ public class ApiGateway {
     @Value("${api.auth.password}")
     private String password;
     private final RestTemplate restTemplate;
+
+    public Integer getMembersCount(String channelId){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBasicAuth(username, password);
+        HttpEntity<Object> requestEntity = new HttpEntity<>(channelId, headers);
+        ResponseEntity<HashMap> responseEntity = restTemplate.exchange(
+                BASEURL.concat("/members/"+channelId+"/count"),
+                HttpMethod.GET,
+                requestEntity,
+                HashMap.class
+        );
+        return Integer.valueOf(responseEntity.getBody().get("count").toString());
+    }
+
+
+
+
+
 
 
 
