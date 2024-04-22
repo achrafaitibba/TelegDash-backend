297d1966729ab6182203067d054850547fa2e0ad
achrafaitibba <aitibbaachraf@gmail.com>
2024-04-22 07:19:52 +0100

    added channel niche attribute for further filtering/classification for another feature that may come in v2, added some niches

--- src/main/java/net/techbridges/telegdash/model/Channel.java
+++ src/main/java/net/techbridges/telegdash/model/Channel.java
@@ -1,18 +1,19 @@
 package net.techbridges.telegdash.model;
 
 import jakarta.persistence.*;
+import net.techbridges.telegdash.model.enums.Niche;
 
 import java.util.List;
+
 
 @Entity
 public class Channel {
     @Id
     private Long id;
     private String name;
+    private List<Niche> niches;
     private String description;
     private Long membersCount;
-    @OneToMany
-    private List<Member> members;
     @ManyToOne
     private Account channelOwner;
     @OneToMany
