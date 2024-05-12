5bee5ae83e897a12836866d7de29a22aa4925036
achrafaitibba <aitibbaachraf@gmail.com>
2024-05-12 11:29:49 +0100

    payment controller done

---
+++ src/main/java/net/techbridges/telegdash/paymentService/paypal/controller/PaymentController.java
@@ -0,0 +1,30 @@
+package net.techbridges.telegdash.paymentService.paypal.controller;
+
+import lombok.RequiredArgsConstructor;
+import net.techbridges.telegdash.paymentService.paypal.dto.CreateSubscriptionRequest;
+import net.techbridges.telegdash.paymentService.paypal.dto.ReviseSubscriptionRequest;
+import net.techbridges.telegdash.paymentService.paypal.model.Subscription;
+import net.techbridges.telegdash.paymentService.paypal.service.SubscriptionService;
+
+@RequiredArgsConstructor
+public class PaymentController {
+
+    private final SubscriptionService subscriptionService;
+
+
+    public Subscription createSubscription(CreateSubscriptionRequest request) throws Exception {
+        return subscriptionService.createSubscription(request);
+    }
+
+    public Subscription getSubscriptionDetails(String subscriptionId) throws Exception {
+        return subscriptionService.getSubscription(subscriptionId);
+    }
+
+    public String approveSubscription(Subscription subscription) throws Exception {
+        return subscriptionService.approveSubscription(subscription);
+    }
+
+    public Object reviseSubscription(ReviseSubscriptionRequest request) throws Exception {
+        return subscriptionService.reviseSubscription(request);
+    }
+}
