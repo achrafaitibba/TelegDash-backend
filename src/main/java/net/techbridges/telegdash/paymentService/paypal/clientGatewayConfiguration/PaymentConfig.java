f9dafcb149da68e0522e8df4f221b712a6f530a4
achrafaitibba <aitibbaachraf@gmail.com>
2024-05-12 11:22:55 +0100

    removed unused code

--- src/main/java/net/techbridges/telegdash/paymentService/paypal/clientGatewayConfiguration/PaymentConfig.java
+++ src/main/java/net/techbridges/telegdash/paymentService/paypal/clientGatewayConfiguration/PaymentConfig.java
@@ -11,25 +11,12 @@ public class PaymentConfig {
     @Value("${payment.paypal.api.base-url}")
     private String basUrl;
 
-
     @Bean
     public BaseUrl baseUrl(){
         BaseUrl baseUrl = new BaseUrl();
         baseUrl.setBaseUrl(basUrl);
         return baseUrl;
     }
-
-//    @Bean
-//    public HttpHeaders httpHeaders() {
-//        HttpHeaders headers = new HttpHeaders();
-//        //headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-//        headers.setBasicAuth(clientId, secret);
-//        //headers.add("Authorization", "Basic " + Base64.getEncoder().encodeToString((clientId + ":" + secret).getBytes()));
-//        return headers;
-//    }
-
-
-
 
 
 }
