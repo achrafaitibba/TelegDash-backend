b6b175768302e3369812d496f8f36767bfcb5119
achrafaitibba <aitibbaachraf@gmail.com>
2024-07-09 22:57:32 +0100

    removed unused method on member repo

--- src/main/java/net/techbridges/telegdash/repository/MemberRepository.java
+++ src/main/java/net/techbridges/telegdash/repository/MemberRepository.java
@@ -1,9 +1,7 @@
 package net.techbridges.telegdash.repository;
 
-import net.techbridges.telegdash.model.Channel;
 import net.techbridges.telegdash.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    int countMembersByChannel(Channel channel);
 }
