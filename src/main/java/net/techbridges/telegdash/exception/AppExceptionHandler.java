c8a96a8f8929b957eb9d1e351c1acf969d9c0c4e
achrafaitibba <aitibbaachraf@gmail.com>
2024-07-18 23:06:31 +0100

    handling uncostumized error occured on runtime and not handled manually

--- src/main/java/net/techbridges/telegdash/exception/AppExceptionHandler.java
+++ src/main/java/net/techbridges/telegdash/exception/AppExceptionHandler.java
@@ -2,9 +2,14 @@ package net.techbridges.telegdash.exception;
 
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.io.Serializable;
@@ -23,5 +28,31 @@ public class AppExceptionHandler extends ResponseEntityExceptionHandler {
                 ZonedDateTime.now(ZoneId.of("GMT"))
         );
         return new ResponseEntity<>(apiException, e.getHttpStatus());
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request) {
+
+        StringBuilder errorMessage = new StringBuilder();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String message = error.getDefaultMessage();
+            errorMessage.append(message).append(", ");
+        });
+
+        errorMessage.replace(errorMessage.length() - 2, errorMessage.length(), ".");
+        String finalErrorMessage = errorMessage.toString();
+
+        ApiException apiException = new ApiException(
+                finalErrorMessage,
+                HttpStatus.BAD_REQUEST,
+                HttpStatus.BAD_REQUEST.value(),
+                ZonedDateTime.now(ZoneId.of("GMT"))
+        );
+
+        return new ResponseEntity<>(apiException, HttpStatus.BAD_REQUEST);
     }
 }
