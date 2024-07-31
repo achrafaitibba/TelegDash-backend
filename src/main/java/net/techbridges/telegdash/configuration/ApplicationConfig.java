620d56069542c7d03e1994c252635c83def88039
achrafaitibba <aitibbaachraf@gmail.com>
2024-07-31 21:46:17 +0100

    added jackson dependency

--- src/main/java/net/techbridges/telegdash/configuration/ApplicationConfig.java
+++ src/main/java/net/techbridges/telegdash/configuration/ApplicationConfig.java
@@ -8,21 +8,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.Collections;
 
 @Configuration
 public class ApplicationConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        RestTemplate restTemplate = new RestTemplate();
-        HttpMessageConverter<Object> converter = new MappingJackson2HttpMessageConverter();
-        restTemplate.setMessageConverters(Collections.singletonList(converter));
-        return restTemplate;
+        return new RestTemplate();
     }
 
     @Bean
